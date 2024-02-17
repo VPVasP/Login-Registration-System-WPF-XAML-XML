8fa9c23686b4efcfa6a284a49d5fef69c6b63b82
Vp <vasilhspinis@gmail.com>
2024-02-17 14:24:21 +0200

    InitialCommit

---
+++ WPFXMLProject/Login-Registration-WPF-XML/LoginRegisterManager.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Login_Registration_WPF_XML
+{
+    public class UserInfoList
+    {
+        public List<UserInfo> UsersInfoList { get; set; } = new List<UserInfo>();
+    }
+
+    public class UserInfo
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+    internal class LoginRegisterManager
+    {
+        private UserInfoList userInfoList;
+        private string userSavedFile = "userData.xml";
+
+        public LoginRegisterManager()
+        {
+            userInfoList = new UserInfoList();
+            LoadUserData();
+        }
+
+        // register method
+        public void Register(string registerUsername, string registerPassword)
+        {
+            if (DoesUserExistRegister(registerUsername))
+            {
+                Debug.WriteLine("Username already exists");
+            }
+            else
+            {
+                UserInfo newUser = new UserInfo()
+                {
+                    Username = registerUsername,
+                    Password = registerPassword,
+                };
+
+                Debug.WriteLine("Registered New User, Also Welcome " + registerUsername);
+                userInfoList.UsersInfoList.Add(newUser);
+                SaveUserData();
+            }
+        }
+
+        // login method
+        public void Login(string loginUsername, string loginPassword)
+        {
+            if (DoesUserExistLogin(loginUsername, loginPassword))
+            {
+                Debug.WriteLine("You are now logged in " + loginUsername);
+            }
+            else
+            {
+                Debug.WriteLine("Wrong Username or Password");
+            }
+        }
+
+        // checking if a username already exists in registration method
+        public bool DoesUserExistRegister(string username)
+        {
+            return userInfoList.UsersInfoList.Exists(user => user.Username == username);
+        }
+
+        // checking if a username already exists in login method
+        public bool DoesUserExistLogin(string username, string password)
+        {
+            return userInfoList.UsersInfoList.Exists(user => user.Username == username && user.Password == password);
+        }
+
+        // save data to XML
+        private void SaveUserData()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(UserInfoList));
+            using (TextWriter writer = new StreamWriter(userSavedFile))
+            {
+                serializer.Serialize(writer, userInfoList);
+            }
+        }
+
+        // load data from XML
+        private void LoadUserData()
+        {
+            if (File.Exists(userSavedFile))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(UserInfoList));
+                using (TextReader reader = new StreamReader(userSavedFile))
+                {
+                    userInfoList = (UserInfoList)serializer.Deserialize(reader);
+                }
+            }
+        }
+    }
+}
---
+++ WPFXMLProject/Login-Registration-WPF-XML/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace Login_Registration_WPF_XML
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private LoginRegisterManager manager;
+        public MainWindow()
+        {
+            InitializeComponent();
+            manager = new LoginRegisterManager();
+        }
+        private void RegisterUser(object sender, RoutedEventArgs e)
+        {
+            string registerUsername = UsernameTextBox.Text;
+            string registerPassword = PasswordBox.Password;
+
+            if (string.IsNullOrEmpty(registerUsername) || string.IsNullOrEmpty(registerPassword))
+            {
+                Debug.WriteLine("Please provide username and password");
+            }
+            //if the register username exists we show a message
+            if (manager.DoesUserExistRegister(registerUsername))
+            {
+                Debug.WriteLine("Username Already Exists");
+                UserExistsMessage.Text = "Username Already Exists";
+                UserExistsMessage.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                manager.Register(registerUsername, registerPassword);
+
+                //hide the registration and login panel
+                RegistrationLoginPanel.Visibility = Visibility.Collapsed;
+
+                //display the registration message 
+                RegistrationMessage.Text = "Registered New User " + registerUsername + " Welcome to the application";
+                RegistrationMessage.Visibility = Visibility.Visible;
+                UserExistsMessage.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void LoginUser(object sender, RoutedEventArgs e)
+        {
+            string loginUsername = UsernameTextBox.Text;
+            string loginPassword = PasswordBox.Password;
+            if (string.IsNullOrEmpty(loginUsername) || string.IsNullOrEmpty(loginPassword))
+            {
+                Debug.WriteLine("Please provide username and password");
+                UserExistsMessage.Visibility = Visibility.Visible;
+                UserExistsMessage.Text = "Please provide username and password";
+            }
+            else
+            //we login,enable and disable the correct messages
+            {
+                manager.Login(loginUsername, loginPassword);
+                RegistrationLoginPanel.Visibility = Visibility.Collapsed;
+                LoginMessage.Text = "You are now logged in " + loginUsername + " Welcome to the application";
+                LoginMessage.Visibility = Visibility.Visible;
+                UserExistsMessage.Visibility = Visibility.Collapsed;
+            }
+        }
+        //we exit the application
+        private void ExitApplication(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
